b3b25318389b58cf5bb3bee2b93a0f68f3e20717
Purushottam <pallipurushotham333@gmail.com>
2025-02-24 15:17:50 +0530

    Initial commit

---
+++ team-showcase/src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from 'react'
+import TeamMemberCard from './TeamMemberCard.jsx'
+
+function App() {
+  
+}
+
+export default App
---
+++ team-showcase/src/TeamMemberCard.jsx
@@ -0,0 +1,9 @@
+function TeamMemberCard(prop){
+    return <>
+    <h1>Team Member Name: {prop.name}</h1>
+    <h3>Job title: {prop.title}</h3>
+
+    </>
+}
+
+export default TeamMemberCard;
