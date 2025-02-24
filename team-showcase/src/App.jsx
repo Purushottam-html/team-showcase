7e606ddff89bb885d8d6cb70d2dd63958344f93c
Purushottam <pallipurushotham333@gmail.com>
2025-02-24 15:44:16 +0530

    Update App.jsx

--- team-showcase/src/App.jsx
+++ team-showcase/src/App.jsx
@@ -2,7 +2,24 @@ import { useState } from 'react'
 import TeamMemberCard from './TeamMemberCard.jsx'
 
 function App() {
-  
+  return(
+    <div className='team-container'>
+
+      <h1>our team</h1>
+      <div className='team-members'>
+        <TeamMemberCard
+        name = "john doe"
+        jobTitle="software engineer"
+        />
+        <TeamMemberCard
+        name = "asdsadas"
+        jobTitle="ux designer"
+        />
+
+      </div>
+      
+    </div>
+  )
 }
 
 export default App
