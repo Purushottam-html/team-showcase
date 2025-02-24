3b4d7c66e618dc42f5e26fd7803d479157386282
Purushottam <pallipurushotham333@gmail.com>
2025-02-24 15:45:15 +0530

    Update TeamMemberCard.jsx

--- team-showcase/src/TeamMemberCard.jsx
+++ team-showcase/src/TeamMemberCard.jsx
@@ -1,9 +1,13 @@
-function TeamMemberCard(prop){
-    return <>
-    <h1>Team Member Name: {prop.name}</h1>
-    <h3>Job title: {prop.title}</h3>
+import React from "react";
 
-    </>
+const TeamMemberCard = ({name , jobTitle}) => {
+    return(
+        <div className="member-card">
+            <h3>{name}</h3>
+            <p>{jobTitle}</p>
+
+        </div>
+    )
 }
 
 export default TeamMemberCard;
